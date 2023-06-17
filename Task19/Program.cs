de5c84b52f678410ddbf6eeb08da925ec52e7647
AndMay <am@42.ru>
2023-06-17 22:20:59 +0700

    Task19  is complete

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,24 +1,35 @@
-﻿
-System.Console.WriteLine("Введите  пятизначное число ");
-string number = Console.ReadLine();
-int numberYN  = Convert.ToInt32(number);  
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
-bool polindrome = true;
+string number;
 
-for (int i = 0; i < number.Length/2; i++)
+do
 {
-    if (number[i] <> number[number.Length - i - 1])
+    System.Console.Write("Введите пятизначное число ");
+    number = Console.ReadLine();             // считываем число в строковую переменную
+    int numberYN = Convert.ToInt32(number);  // так проверим что точно число  :)
+
+} while (number.Length != 5); // "выбиваем" из пользователя именно Пятизначное число 
+
+bool polindrome = true;  // предположим что число полиндром
+
+for (int i = 0; i < number.Length / 2; i++)         // цикл до середины строки для перебора символов по парно    
+{
+    if (number[i] != number[number.Length - i - 1]) // если символы не одинаковы
     {
-        polindrome = false;
-        break;
+        polindrome = false; // указываем, что число не полиндром
+        break;              // и прерываем цикл
     }
 }
 
-if polindrome
+if (polindrome)
 {
-    System.Console.WriteLine($"Число {number} полиндром.");
+    System.Console.WriteLine($"-> да - число {number} полиндром.");
 }
 else
 {
-    System.Console.WriteLine($"Число {number} НЕ полиндром.");
+    System.Console.WriteLine($"-> нет - число {number} НЕ полиндром.");
 }
