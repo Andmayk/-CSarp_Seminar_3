c2f1ae2af6121bd62b61da184da837bcbdfe01d2
AndMay <am@42.ru>
2023-06-17 21:40:00 +0700

    before first start task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.WriteLine("Введите  пятизначное число ");
+string number = Console.ReadLine();
+int numberYN  = Convert.ToInt32(number);  
+
+bool polindrome = true;
+
+for (int i = 0; i < number.Length/2; i++)
+{
+    if (number[i] <> number[number.Length - i - 1])
+    {
+        polindrome = false;
+        break;
+    }
+}
+
+if polindrome
+{
+    System.Console.WriteLine($"Число {number} полиндром.");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} НЕ полиндром.");
+}
